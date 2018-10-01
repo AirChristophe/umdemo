5ab6a2f57178a5ea97d9ecbefdc39f3ea61848f0
cyduval <cyrille@videodesk.com>
2018-10-01 11:20:16 +0200

    auth

--- app/screens/MyApps/index.js
+++ app/screens/MyApps/index.js
@@ -4,8 +4,8 @@ import { connect } from 'react-redux';
 import isNetwork from 'umdemo/utils/isNetwork';
 import NoNetwork from 'app/components/NoNetwork';
 import { View, StyleSheet } from 'react-native';
-import { Text } from 'react-native-elements';
-import { Constants } from 'expo';
+import { Button, Text } from 'react-native-elements';
+import { AuthSession, Constants } from 'expo';
 import Header from 'app/components/Header';
 import Loading from 'app/components/Loading';
 
@@ -17,11 +17,48 @@ class MyApps extends React.Component {
         this.state = {
             loading: false,
             data: false,
+            result: false,
         };
+        this.connectStrava = this.connectStrava.bind(this);
+    }
+
+    connectStrava = async () => {
+        console.log(777777);
+        let redirectUrl = AuthSession.getRedirectUrl();
+        console.log(redirectUrl);
+        // alert(redirectUrl);
+        console.log(8888888);
+        let result = await AuthSession.startAsync({
+            authUrl:
+            `https://www.strava.com/oauth/authorize` +
+            `?client_id=28916` +
+            `&response_type=code` +
+            `&redirect_uri=${encodeURIComponent(redirectUrl)}`,
+        });
+        console.log(result);
+        // console.log(JSON.parse(result));
+        // console.log(result.params);
+        // console.log(result.params.code);
+        this.setState({ result });
+    }
+    
+    code() {
+        const { result } = this.state;
+
+        if (result) {
+            console.log(111);
+            console.log(result);
+            console.log(result.params);
+            return `token => ${result.params.code}`;
+        }
+        console.log(222);
+        return '';
+
     }
 
     render() {
         const { app } = this.props;
+        
         const { loading } = this.state;
 
 
@@ -52,9 +89,18 @@ class MyApps extends React.Component {
             />
             <View style={styles.container}>
 
-                <Text>
-                aaaaaaaa
-                </Text>
+                <Button
+                    title='Connect strava' 
+                    buttonStyle={styles.strava}
+                    onPress={this.connectStrava}
+                    containerViewStyle={{width: '100%', marginLeft: 0}}
+                    // onPress={() => this.props.navigation.navigate('Reset')}
+                />
+                <View>
+                    <Text>
+                        { this.code() }
+                    </Text>
+                </View>
 
             </View>   
         </View>
@@ -77,6 +123,11 @@ class MyApps extends React.Component {
       height: '100%',
       width: '100%',
     },
+    strava: {
+        backgroundColor: '#ea4a33',
+        margin: 20,
+        padding: 10, 
+    }
   });
 
 
