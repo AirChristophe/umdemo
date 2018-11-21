21cd3124a11a564ced797319ae537f854248af10
Jules Bertrand <jules.bertrand@trimaran.com>
2018-11-21 12:59:55 +0100

    -

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -9,7 +9,7 @@ export default class HeaderComponent extends React.Component  {
             leftComponent={{ size: 30, icon: this.props.icon ? this.props.icon : 'keyboard-backspace' , color: '#fff', onPress: this.props.onPress }}
             centerComponent={{ text: this.props.text, style: { color: '#fff' } }}
             statusBarProps={{ barStyle: 'light-content' }}
-            outerContainerStyles={{ width: '100%'  }}
+            outerContainerStyles={{ width: '100%' }}
             innerContainerStyles={{  }}
             backgroundColor={colors.main}
         />
